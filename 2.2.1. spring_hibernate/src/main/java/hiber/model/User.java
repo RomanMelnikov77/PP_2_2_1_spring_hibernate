f218a63c93e968364a55b9007ce813fc28da39aa
RomanMelnikov77 <79645773155@yandex.ru>
2022-07-05 21:37:44 +0300

    2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,36 +1,44 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
 public class User {
-
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
-
    @Column(name = "name")
    private String firstName;
-
    @Column(name = "last_name")
    private String lastName;
-
    @Column(name = "email")
    private String email;
 
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn (name = "car_id", referencedColumnName = "id")
+   private Car car;
+
    public User() {}
-   
-   public User(String firstName, String lastName, String email) {
+
+   public User(String firstName, String lastName, String email , Car car) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.car = car;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+   public void setCar(Car car) {
+      this.car = car;
    }
 
    public Long getId() {
       return id;
    }
-
    public void setId(Long id) {
       this.id = id;
    }
@@ -38,7 +46,6 @@ public class User {
    public String getFirstName() {
       return firstName;
    }
-
    public void setFirstName(String firstName) {
       this.firstName = firstName;
    }
@@ -46,7 +53,6 @@ public class User {
    public String getLastName() {
       return lastName;
    }
-
    public void setLastName(String lastName) {
       this.lastName = lastName;
    }
@@ -54,8 +60,32 @@ public class User {
    public String getEmail() {
       return email;
    }
-
    public void setEmail(String email) {
       this.email = email;
    }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      User user = (User) o;
+      return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email) && Objects.equals(car, user.car);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(id, firstName, lastName, email, car);
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              ", car=" + car +
+              '}';
+   }
 }
+
